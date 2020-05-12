903a4ed88a8c16659c270f5d1f251e14d70bdc6b
cognitonow <cognito.learning.now@gmail.com>
2020-05-12 14:56:52 +0200

    sdf
    sdf

---
+++ story_content/user.js
@@ -0,0 +1,119 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ky1pDjXdZm":
+        Script1();
+        break;
+      case "6nU0JUXGTPj":
+        Script2();
+        break;
+      case "6WOonLfecZl":
+        Script3();
+        break;
+      case "6jFrFsc42Tt":
+        Script4();
+        break;
+      case "5bDoGhd7IBe":
+        Script5();
+        break;
+      case "6CfSTB8KYQx":
+        Script6();
+        break;
+      case "6XbBu79MQWE":
+        Script7();
+        break;
+      case "5kXOtKgQRC1":
+        Script8();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+FlashNaviListener = function(param){
+   player.SetVar("navigationControl",param);
+   player.SetVar("navigationControl",null);
+}
+
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+FlashNaviListener = function(param){
+   player.SetVar("navigationControl",param);
+   player.SetVar("navigationControl",null);
+}
+
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+FlashNaviListener = function(param){
+   player.SetVar("navigationControl",param);
+   player.SetVar("navigationControl",null);
+}
+
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+FlashNaviListener = function(param){
+   player.SetVar("navigationControl",param);
+   player.SetVar("navigationControl",null);
+}
+
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+
+FlashNaviListener = function(param){
+   player.SetVar("navigationControl",param);
+   player.SetVar("navigationControl",null);
+}
+
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+FlashNaviListener = function(param){
+   player.SetVar("navigationControl",param);
+   player.SetVar("navigationControl",null);
+}
+
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+
+FlashNaviListener = function(param){
+   player.SetVar("navigationControl",param);
+   player.SetVar("navigationControl",null);
+}
+
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+
+FlashNaviListener = function(param){
+   player.SetVar("navigationControl",param);
+   player.SetVar("navigationControl",null);
+}
+
+}
+
